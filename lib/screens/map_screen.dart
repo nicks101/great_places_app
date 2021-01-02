f2428e47cefe40e12d1fcac3b8e64f93a140e786
Nikki Goel <gupta.nikkigupta@gmail.com>
2021-01-02 22:11:13 +0530

    wip: map screen

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -5,12 +5,14 @@ import '../models/place.dart';
 
 class MapScreen extends StatefulWidget {
   final PlaceLocation initialLocation;
+  final bool isSelecting;
 
   MapScreen({
     this.initialLocation = const PlaceLocation(
       latitute: 35.88,
       longitute: 82.22,
     ),
+    this.isSelecting = false,
   });
 
   @override
@@ -18,11 +20,29 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Locations'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: _pickedLocation == null
+                ? null
+                : () {
+                    Navigator.of(context).pop(_pickedLocation);
+                  },
+          ),
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
@@ -32,6 +52,15 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 16,
         ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? null
+            : {
+                Marker(
+                  markerId: MarkerId('m1'),
+                  position: _pickedLocation,
+                ),
+              },
       ),
     );
   }
